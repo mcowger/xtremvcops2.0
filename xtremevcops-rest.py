5ba05ea424b738d5246ddc186e3fa33c44b6aba6
Matt Cowger <matt@cowger.us>
2014-06-05 21:01:42 -0700

    First working version. * Supports XtremeIO 2.2 and 2.4 * Supports VCOPS 5.x * Supports Python 2.7 or 3.4

--- xtremevcops-rest.py
+++ xtremevcops-rest.py
@@ -1,15 +1,16 @@
 __author__ = 'mcowger'
 
-from pprint import pprint
-from hammock import Hammock as XtremIO
-from docopt import docopt
-import json
 import logging
+import time
 
+from docopt import docopt
+
+from hammock import Hammock as XtremIO
+from hammock import Hammock as Vcops
 
 
 __doc__ = """
-Usage: xtremevcops-rest.py [-h] XMS_IP XMS_USER XMS_PASS VCOPS_IP [--vcops_user] [--vcops_pass] [--quiet] [--debug_level=<level>] [--protocol=<proto>] [--xms_base_path=<base_path>]
+Usage: xtremevcops-rest.py [-h] XMS_IP XMS_USER XMS_PASS VCOPS_IP [--interval=<interval>] [--vcops_user=<user>] [--vcops_pass=<pass>] [--quiet] [--debug_level=<level>] [--protocol=<proto>] [--xms_base_path=<base_path>]
 
 Collect Performance Statistics from an XtremIO array and push them to vCenter Operations
 
@@ -23,28 +24,33 @@ Arguments:
 Options:
     -h --help    show this
     --quiet      print less text
-    --debug_level=<level>    Very verbose debugging [default: INFO]
+    --debug_level=<level>    Very verbose debugging [default: WARN]
     --protocol=<proto>   [http | https] [default: https]
     --xms_base_path=<base_path>  base_path for API operations [default: /api/json/types]
-    --vcops_user  VC Ops User [default: admin]
-    --vcops_pass    VC Ops Password [default: P@ssword1!]
+    --vcops_user=<user>  VC Ops User [default: admin]
+    --vcops_pass=<pass>  VC Ops Password [default: P@ssword1!]
+    --interval=<interval>   Sleep interval between collections [default: 60]
 """
 options = docopt(__doc__, argv=None, help=True, version=None, options_first=False)
 
-if options['--debug_level'] == "DEBUG":
-    logging.basicConfig(level=logging.DEBUG)
-else:
-    logging.basicConfig(level=logging.INFO)
+numeric_level = getattr(logging, options['--debug_level'].upper(), None)
+if not isinstance(numeric_level, int):
+    raise ValueError('Invalid log level: %s' % loglevel)
+logging.basicConfig(level=numeric_level,format='%(asctime)s|%(name)s|%(levelname)s|%(module)s:%(lineno)d|%(message)s')
 
 logger = logging.getLogger(__name__)
+logger.info("Printing received options:")
+logger.info(options)
+
 
 xtremio = XtremIO(options['--protocol']+ "://" + options['XMS_IP'] + options['--xms_base_path'],verify=False, auth=(options['XMS_USER'], options['XMS_PASS']))
 
 
-
 def get_volumes():
     logger.info("Retrieving list of all volumes from XMS")
-    return xtremio.volumes.GET().json()['volumes']
+    volumes =  xtremio.volumes.GET().json()['volumes']
+    logger.info("Got %d volumes from XMS" % len(volumes))
+    return volumes
 
 def get_volume(volume_record):
     logger.info("Retrieving info for volume: " + volume_record['name'])
@@ -58,15 +64,35 @@ def get_brick_list():
     """
     return xtremio.bricks.GET().json()['bricks']
 
+def collect_and_submit():
+    vcops_connection = Vcops_Connection(vcops_ip=options['VCOPS_IP'])
+    for volume in get_volumes():
+        volume_info = get_volume(volume)
+        brick_name = volume_info['content']['sys-id'][1]
+
+        vcops_info = Vcops_Record_Keeper(resourceName='XMS-'+brick_name+"-"+volume['name'],resourceKindKey='XtremeIO-Array',resourceDescription='XtremeIO Array')
+        for metric in volume_info['content'].items():
+            if type(metric[1]) in [type(1),type("")]: #Did we find a str or int?
+                logger.debug("Metric found: brick:volume:metric:value %s:%s:%s:%s" % (brick_name,volume['name'],metric[0],metric[1]))
+                vcops_info.add_metric_observation(entity_name=volume['name'],metric_name=metric[0],value=str(metric[1]))
+        response = vcops_connection.submit_set(vcops_info.first_line,vcops_info.metric_lines)
+        logger.info(response)
+
+
 class Vcops_Connection(object):
-    def __init__(self,vcops_ip="",vcops_user=options['--vcops_user'],vcops_pass=options['--vcops_pass']):
-        super().__init__()
+    def __init__(self,vcops_ip=""):
+        self.vcops_ip = vcops_ip
+        self.vcops = Vcops(options['--protocol']+ "://" + self.vcops_ip + '/HttpPostAdapter/OpenAPIServlet',verify=False,auth=(options['--vcops_user'],options['--vcops_pass']))
+        logger.info("Logging into vcops @ %s as %s" % (self.vcops_ip,options['--vcops_user']))
 
+    def submit_set(self,first_line,metric_lines):
+        logger.debug("Submitting metrics as user: %s:\n %s" % (options['--vcops_user'],first_line+'\n'+metric_lines[0:500]))
+        response = self.vcops.POST(data=first_line+'\n'+metric_lines)
+        return response
 
-
-class Vcops_Record(object):
+class Vcops_Record_Keeper(object):
     def __init__(self,resourceName,resourceKindKey="",identifiers="",resourceDescription=""):
-        super().__init__()
+
         self.resource_name = resourceName
         self.resource_kind_key = resourceKindKey
         self.identifiers = identifiers
@@ -85,7 +111,7 @@ class Vcops_Record(object):
         return first_line
 
     def add_metric_observation(self,entity_name,metric_name,alarm_level=0,alarm_message="",value=''):
-        metric_string = ','.join((entity_name + '|' + metric_name,str(alarm_level),alarm_message,str(self.current_time_millis),value))
+        metric_string = ','.join((metric_name,'','',str(self.current_time_millis),value,str(alarm_level)))
         self.metrics.append(metric_string)
         logger.debug("Added metric: %s" % metric_string)
 
@@ -95,13 +121,11 @@ class Vcops_Record(object):
         import time
         return int(round(time.time() * 1000))
 
-if __name__ == "__main__":
-    vcops_info = Vcops_Record(resourceName='XMS-'+ options['XMS_IP'],resourceKindKey='XtremeIO-Array',resourceDescription='XtremeIO Array')
-    for volume in get_volumes():
-        volume_info = get_volume(volume)
-        brick_name = volume_info['content']['sys-id'][1]
-        for metric in volume_info['content'].items():
-            if type(metric[1]) in [type(1),type("")]: #Did we find a str or int?
-                logger.debug("Metric found: brick:volume:metric:value %s:%s:%s:%s" % (brick_name,volume['name'],metric[0],metric[1]))
-                vcops_info.add_metric_observation(entity_name=volume['name'],metric_name=metric[0],value=str(metric[1]))
 
+
+if __name__ == "__main__":
+    while True:
+        logger.warn("Starting Collection Run")
+        collect_and_submit()
+        logger.warn("Sleeping for %d seconds until next run" % int(options['--interval']))
+        time.sleep(int(options['--interval']))
